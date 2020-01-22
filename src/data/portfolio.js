14061aa5cbf6c05d8f4e3365b0d789d7efcad7e5
k3llydev <hello@k3lly.dev>
2020-01-22 09:31:50 -0600

    Feature(portfolio): Updated payload

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,108 +1,43 @@
 export default {
-    experience: [
-      {
-        name: 'BBVA',
-        experience: [
-          {
-            job: 'Front-end Software Engineer - Holding Mx',
-            startDate: '25/09/2019',
-            description: 'Develop features for GloMo, the future global customers application. The best banking app in the world.',
-            labours: [
-                'Code, debug and mantain DIY offers for customers', 
-                'Reply code to different geolocations (PE, MX, UY, etc.)',
-                'Fast learning, efficient development'
-            ],
-            tools: ['CELLS', 'JavaScript', 'HTML', 'Polymer', 'SASS', 'Cucumber', 'Mocha', 'E2E', 'Unit Testing', 'CI', 'Jenkins', 'Mobile', 'Agile (Scrum)']
-          },
-          {
-            job: 'Developer at BEyG - Business Process Engineering',
-            startDate: '08/07/2019',
-            endDate: '20/09/2019',
-            description: 'Developed a public web portal for customers to obtain BBVA Net Cash.',
-            labours: [
-                'Develop from scratch an entire website in CELLS', 
-                'Maintain a great balance between technical & non-technical speak',
-                'Agile development (Scrum)',
-
-            ],
-            tools: ['CELLS', 'JavaScript', 'HTML', 'Polymer', 'Unit Testing', 'Jenkins', 'CI', 'CD']
-          }
-        ]
-      },
-      {
-        name: 'Ironbit - Digital Experiences SAPI de CV',
-        experience: [
-            {
-                job: 'Polymer Developer',
-                startDate: '10/06/2019',
-                description: 'Contractor to BBVA'
-            }
-        ]
-      },
-      {
-        name: 'Vibocreando',
-        experience: [
-            {
-                job: 'Web Master',
-                startDate: '01/02/2019',
-                endDate: '01/06/2019',
-                description: 'Lead of web technologies, responsable of providing high efficiency solutions to different customers',
-                labours: [],
-                tools: ['JavaScript', 'HTML', 'CSS', 'PHP', 'Node.js', 'RESTFul APIs', 'Web Sockets']
-            }
-        ]
-      },
-      {
-        name: 'Integracion GX',
-        experience: [
-            {
-                job: 'Junior Web Developer',
-                startDate: '01/03/2018',
-                endDate: '01/01/2019',
-                labours: [
-                    'Implement features GeneXus can not code by itself',
-                    'Review and optimize JavaScript exported files',
-                    'Use SAT APIs for billing'
-                ],
-                tools: ['JavaScript', 'HTML', 'CSS', 'C++', 'GeneXus', 'Rest APIs', 'SQL']
-            }
-        ]
-      }
-    ],
-    skills: [
-        'JavaScript', 'Python', 'HTML', 'CSS', 'Web Components', 'SASS', 'CELLS (BBVA)', 'webpack', 'nodejs', 'React', 'Polymer', 'LitElement', 'C++', 'E2E', 'JSON', 'RESTFul APIs',
+  projects: [
+    {
+      name: 'this.website',
+      description: 'My professional website for my portfolio. Contains most part of my work, should also be public (even the code) for everyone.',
+      tasks: [
+        'Implemented CI/CD for automatic deploys and testing',
+        'Used Polymer 3 + LitElement, just because they are compatible and awesome',
+        'Polymer is not dead. Has a huge potential which should be exploited',
+        'Planning to integrate other frameworks, just for fun!'
+      ],
+      technologies: ['JavaScript', 'Polymer 3', 'LitElement', 'CI/CD', 'CircleCI', 'CSS', 'Custom Properties'],
+      endDate: 'Beta (already in production)',
+      url: 'https://github.com/k3llydev/bitten-c00kie'
+    },
+    {
+      name: 'bitten-cookie',
+      description: 'A simple PHP web application to prove client-side is a lot more vulnerable than we thought.',
+      tasks: [
+        'Used a simple script with tampermonkey to gradually obtain data from the browser',
+        'A PHP script catches this information and stores it',
+        'You can register users, provide them a login and they can view their catched data separately'
+      ],
+      endDate: 'Done',
+      technologies: ['PHP', 'JavaScript', 'XSS Exploits', 'AJAX as POST', 'IT Security'],
+      url: 'https://github.com/k3llydev/bitten-c00kie'
+    }
+  ],
+  skills: [
+    'JavaScript', 'Python', 'HTML', 'CSS', 'Web Components', 'SASS', 'CELLS (BBVA)', 'webpack', 'nodejs', 'React', 'Polymer', 'LitElement', 'C++', 'E2E', 'JSON', 'RESTFul APIs',
         'CI/CD', 'Jenkins', 'Atomic Design', 'CLIs', 'Mocha', 'Sinon', 'Chai', 'Jest', 'Angular', 'SQL'
-    ],
-    education: [
-      {
-        place: 'Systems Automation Engineering, University of Matamoros',
-        startDate: '01/09/2016',
-        endDate: '01/03/2018',
-        labours: [],
-        tools: ['OOP', 'Data Structure', '.NET', 'Visual Basic', 'C++', 'Algebra', 'Statistics', 'HTML', 'SQL']
-      },
-      {
-        place: 'Aspirant of Computer Science degree & PhD',
-        startDate: 'Anytime',
-        endDate: ' ',
-        description: 'Willing for a job that cares about my personal and intellectual growth',
-        labours: [],
-      },
-      {
-        place: 'Scrum Master Certified',
-        tools: ['SMC']
-      }
-    ],
-    projects: [
-      {
-        name: 'bitten-c00kie',
-        briefDescription: 'A simple web app made in PHP to prove how client-side vulnerabilites are still an issue.',
-        url: 'https://github.com'
-      },
-      {
-          name: 'this.website',
-          briefDescription: 'My professional website involved with Node, Polymer, LitElement, CI/CD and further more.',
-          url: 'https://github.com/k3llydev/k3llydev-website'
-      }
-    ]
-  }
+  ],
+  certifications: [
+    {
+      title: 'Scrum Master Certified (SMC)',
+      place: 'Scrum Study',
+      description: '@ScrumStudy',
+      endDate: 'Certified',
+      labours: [],
+      tools: ['Agile']
+    }
+  ]
+}
