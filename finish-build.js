c210718077dd10f2f53f0c7c5ff67cce6550ead5
Luis Kelly Diaz <hello@k3lly.dev>
2021-06-09 12:12:04 -0500

    Fix: Changes to gh-pages deployment

--- finish-build.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-// ADD CNAME FILE FOR DOMAIN
-fs.writeFile('./build/default/CNAME', 'k3lly.dev', function(err) {
-    if(err) {
-        return console.log(err);
-    }
-});
